aeb8b831f72316ad668fca6ffc7800b5088c446e
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-05-16 19:58:11 +0530

    adding all new blog code for gatsby setup

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE THIRD PAGE</h1>
+  </Layout>
+)
+
+export default ThirdPage
